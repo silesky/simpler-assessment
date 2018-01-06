8fcbdc2304b5975016419ef667a685cdd452e068
Seth Silesky <seth.silesky@gmail.com>
2018-01-06 13:06:27 -0600

    routing works

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,13 @@
 
-import 'App/normalize.css'
-
 import React from 'react'
 import { Switch, Route } from 'react-router'
-import Home from 'Home'
-import Sessions from 'Sessions'
-import Session from 'Session'
-import NewsOverview from 'NewsOverview'
-import styles from './styles.css'
+import Home from '../../screens/Home'
 
 const App = (): React.Element<any> =>
-  (<div className={styles.wrapper}>
-    <H1>Hello World</H1>
+  (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/sessions" component={Sessions} />
-      <Route exact path="/sessions/:sessionName/" component={Session} />
-      <Route exact path="/news/:page?/" component={NewsOverview} />
     </Switch>
-  </div>)
+  )
 
 export default App
---
+++ src/screens/Home/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.css'
+const Home = () =>
+  <div className='wrapper'>
+    <h1>Hello Home</h1>
+  </div>
+
+export default Home
